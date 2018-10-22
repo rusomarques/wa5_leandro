29cd98ac6c412edcd082f2e7cf20c7cd26e1020c
rusolm <leandromarquesp88@gmail.com>
2018-10-22 10:19:51 +0200

    apiClient-service: add getTopics

--- client/src/app/api-client.service.ts
+++ client/src/app/api-client.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient }    from '@angular/common/http';
+import { Observable } from "rxjs";
+import { Topic } from './topic';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiClientService {
 
-  constructor() { }
+  ulrTopics: string = 'http://localhost:3000/topics';
+
+  constructor(private http: HttpClient) { }
+
+  getAllTopics(): Observable<Topic[]> {
+    return this.http.get<Topic[]>(this.ulrTopics);
+  }
+
 }
