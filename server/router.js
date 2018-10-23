c7bc346182123effe38aa3c6684d84b413be0f0f
rusolm <leandromarquesp88@gmail.com>
2018-10-23 21:33:18 +0200

    server: fix bug delete method

--- server/router.js
+++ server/router.js
@@ -1,10 +1,10 @@
-const Router = require('koa-router');
+const Router = require("koa-router");
 const router = new Router();
-const topic = require('./controllers/topic');
+const topic = require("./controllers/topic");
 
 router
-  .get('/topics', topic.getAll)
-  .post('/topic', topic.setTopic)
-  .del('/topic/:id', topic.deleteTopic);
+  .get("/topics", topic.getAll)
+  .post("/topic", topic.setTopic)
+  .delete("/topic/:id", topic.deleteTopic);
 
 module.exports = router;
